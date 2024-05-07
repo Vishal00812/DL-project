fcb6fb6c2751d770dc83f9277649be087185235d
Vishal00812 <vktawesome568@gmail.com>
2024-05-07 22:53:32 +0530

    training added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -25,3 +25,14 @@ class PrepareCallbacksConfig:
     root_dir: Path
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath : Path = Path("artifacts\prepare_callbacks\checkpoint_dir\model.h5\model.keras")
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
